68aea57967b33ab332b6cdaecc42c60d76d1c333
subin-sabu <subincs2023@gmail.com>
2024-11-23 22:38:13 +0530

    finished 2 qns

--- learn.js
+++ learn.js
@@ -10,3 +10,19 @@ for (let i = 0; i < randomStr.length; i++) {
 }
 
 console.log(result); //bgnig
+
+// method: 2
+const res = randomStr.split('').filter((_, index) => index % 2 === 0).join('');
+
+console.log(res); //bgnig
+
+
+/* Q2. Iterate through the first 10 numbers (0 to 9) and, in each iteration, print the sum of the current number and the previous number */
+
+let previous = 0;
+
+for (let current = 0; current < 10; current++) {
+  let sum = current + previous; 
+  console.log(`current ${current} + previous ${previous} = sum ${sum}`);
+  previous = current;
+}
